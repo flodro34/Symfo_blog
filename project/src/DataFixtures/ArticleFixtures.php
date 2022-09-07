ef478e8df5605eb98ff3dda9e03456520c844f95
Flo_d_R <bwb-f.de-rosso@fondespierre.com>
2022-09-07 10:34:56 +0000

    add security

--- project/src/DataFixtures/ArticleFixtures.php
+++ project/src/DataFixtures/ArticleFixtures.php
@@ -5,20 +5,61 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Article;
+use App\Entity\Category;
+use App\Entity\Comment;
+
 
 class ArticleFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        for($i =1; $i<=10; $i++){
+        $faker = \Faker\Factory::create('fr_FR');
+
+        //Créer 3 fake catégory
+        for($i =1; $i<=3; $i++){
+            $category = new Category();
+            $category->setTitle($faker->sentence())
+                     ->setDescription($faker->paragraph());
+            
+            $manager->persist($category);
+        }
+
+        //Créer entre 4 et 6 articles
+        for($j = 1; $j <= mt_rand(4, 6); $j++){
             $article = new Article();
-            $article->setTitle("Titre de l'article n° $i")
-                ->setContent("<p>Contenu de l'article n°$i</p>")
-                ->setImage("https://placehold.jp/350x150.png")
-                ->setCreatedAt(new \DateTime());
+
+           
+            $content = '<p>' . join($faker->paragraphs(5), '</p><p>') . '</p>';
+           
+
+            $article->setTitle($faker->sentence())
+                ->setContent($content)
+                ->setImage($faker->imageUrl())
+                ->setCreatedAt($faker->dateTimeBetween('- 6 months'))
+                ->setCategory($category);
 
             $manager->persist($article);
         }
+
+        //on donne des comments à l'article
+        for($k = 1; $k<= mt_rand(4, 10); $k++){
+            $comment = new Comment();
+
+            $content = '<p>' . join($faker->paragraphs(2), '</p><p>') . '</p>';
+
+            $now = new \DateTime;
+            $days = $now->diff($article->getCreatedAt())->days;
+            $minimum = '-' . $days . 'days'; // -100 jours
+
+
+            $comment->setAuthor($faker->name)
+                    ->setContent($content)
+                    ->setCreatedAt($faker->dateTimeBetween($minimum))
+                    ->setArticle($article);
+
+            $manager->persist($comment);
+        }
+       
 
         $manager->flush(); //envoie la requete sql
     }
